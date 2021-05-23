a9f2f61761190b211e8211d0cefa70a4e21fbf2b
Dangopan <82127092+Dangopan@users.noreply.github.com>
2021-05-23 18:39:05 +0900

    panda-bread add

---
+++ panda-bread/introduction.js
@@ -0,0 +1,3 @@
+$(function() {
+    $('.scroll.wpp-list').wrapAll('<div class="scroll-wpp" />').after('<a class="leftbutton" href="#"><i class="fa fa-angle-double-left" aria-hidden="true"></i></a>','<a class="rightbutton" href="#"><i class="fa fa-angle-double-right" aria-hidden="true"></i></a>');
+});
